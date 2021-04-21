a7b9496e83db314b4a9918fb2ccf5beb6a551512
Kamil Moszczyc <likeyouop@gmail.com>
2021-04-21 17:38:59 +0200

    Fix project structure

--- Anticovid/app/src/main/java/com/example/anticovid/ui/login/LoginForm.kt
+++
@@ -1,5 +0,0 @@
-package com.example.anticovid.ui.login
-
-enum class LoginForm {
-    SignIn, SignUp
-}
--- Anticovid/app/src/main/java/com/example/anticovid/ui/login/LoginFormState.kt
+++
@@ -1,9 +0,0 @@
-package com.example.anticovid.ui.login
-
-/**
- * Data validation state of the login form.
- */
-data class LoginFormState(val emailError: Int? = null,
-                          val passwordError: Int? = null,
-                          val repeatedPasswordError: Int? = null,
-                          val isDataValid: Boolean = false)
