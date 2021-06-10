e196c64c2929b9e28669d5baf71d1f5fce5e7b4b
Kamil Moszczyc <likeyouop@gmail.com>
2021-06-10 20:53:08 +0200

    Add bluetooth draft v2

--- app/src/main/java/com/example/anticovid/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/anticovid/ui/main/MainActivity.kt
@@ -1,17 +1,26 @@
 package com.example.anticovid.ui.main
 
 import android.Manifest
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
 import android.content.pm.PackageManager
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.anticovid.R
-import com.example.anticovid.ui.login.*
+import com.example.anticovid.ui.login.LoginActivity
 import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -91,8 +100,14 @@ class MainActivity : AppCompatActivity() {
 
     private fun requestAllPermissions()
     {
-        val isFineLocationDenied = ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)  == PackageManager.PERMISSION_DENIED
-        val isCourseLocationDenied = ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)  == PackageManager.PERMISSION_DENIED
+        val isFineLocationDenied = ActivityCompat.checkSelfPermission(
+            this,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        )  == PackageManager.PERMISSION_DENIED
+        val isCourseLocationDenied = ActivityCompat.checkSelfPermission(
+            this,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+        )  == PackageManager.PERMISSION_DENIED
         val isInternetDenied = ContextCompat.checkSelfPermission(this, Manifest.permission.INTERNET) == PackageManager.PERMISSION_DENIED
 
         val permissions = arrayOf(
@@ -104,5 +119,48 @@ class MainActivity : AppCompatActivity() {
         if (isFineLocationDenied || isCourseLocationDenied ||  isInternetDenied) {
             requestPermissions(permissions, REQUEST_CODE)
         }
+
+        val requestCode = 1;
+        val discoverableIntent: Intent = Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE).apply {
+            putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 3600)
+        }
+        startActivityForResult(discoverableIntent, requestCode)
+        val filter = IntentFilter(BluetoothDevice.ACTION_FOUND)
+        registerReceiver(receiver, filter)
+        val mBtAdapter = BluetoothAdapter.getDefaultAdapter()
+        dicoverDevicesPeriodically(mBtAdapter)
     }
+
+    // Create a BroadcastReceiver for ACTION_FOUND.
+    private val receiver = object : BroadcastReceiver() {
+
+        override fun onReceive(context: Context, intent: Intent) {
+            val action: String? = intent.action
+            Log.wtf("bluetooth", action)
+            when(action) {
+                BluetoothDevice.ACTION_FOUND -> {
+                    val device: BluetoothDevice? =
+                        intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE)
+                    val deviceName = device!!.name
+                    val deviceHardwareAddress = device.address // MAC address
+                    Log.wtf("bluetooth", deviceName + " " + deviceHardwareAddress)
+                }
+            }
+        }
+    }
+
+    fun dicoverDevicesPeriodically(btAdapter:BluetoothAdapter){
+        val mainHandler = Handler(Looper.getMainLooper())
+
+        mainHandler.post(object : Runnable {
+            override fun run() {
+                if (btAdapter.isDiscovering()) {
+                    btAdapter.cancelDiscovery();
+                }
+                btAdapter.startDiscovery();
+                mainHandler.postDelayed(this, 60000)
+            }
+        })
+    }
+
 }
