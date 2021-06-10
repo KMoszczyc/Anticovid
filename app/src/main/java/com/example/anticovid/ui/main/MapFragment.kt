9b2491b543adece095aa8e873dd0d258250324d6
Kuba <kuba180298@onet.eu>
2021-06-10 18:49:23 +0200

    bug fix

--- app/src/main/java/com/example/anticovid/ui/main/MapFragment.kt
+++ app/src/main/java/com/example/anticovid/ui/main/MapFragment.kt
@@ -23,7 +23,7 @@ class MapFragment : Fragment() {
 
     private val callback = OnMapReadyCallback { googleMap ->
         map = googleMap
-        locationRepository = LocationRepository(context!!, map)
+        locationRepository = LocationRepository(requireContext(), map)
     }
 
     override fun onCreateView(
